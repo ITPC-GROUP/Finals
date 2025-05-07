42c2f4660634466e1a059e59a1780b7e1e940fe2
ITPC-GROUP <itpcjenkins@gmail.com>
2025-05-07 23:14:56 +0800

    Create CharCounter.java

---
+++ CharCounter.java
@@ -0,0 +1,6 @@
+public class CharCounter {
+    public static void main(String[] args) {
+        String message = "Hello Jenkins!";
+        System.out.println("Character count: " + message.length());
+    }
+}
