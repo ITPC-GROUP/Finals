4f2d9ef474a18e112c3be1600c45d80c9e2f6aa5
ITPC-GROUP <itpcjenkins@gmail.com>
2025-05-07 23:21:43 +0800

    Update CharCounter.java

--- CharCounter.java
+++ CharCounter.java
@@ -1,6 +1,7 @@
 public class CharCounter {
     public static void main(String[] args) {
         String message = "Hello Jenkins!";
+        System.out.println("Hello Jenkins!");
         System.out.println("Character count: " + message.length());
     }
 }
